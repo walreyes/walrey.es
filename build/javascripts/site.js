3795c28885dd5b2bdf49a4ee9cd1a11e50b436d9
Walter Reyes <wal.reyes.11@gmail.com>
2017-09-28 16:04:00 -0500

    Deploy build site

---
+++ build/javascripts/site.js
@@ -0,0 +1,18 @@
+$( document ).ready(function() {
+  console.log( "Got ya! What are you doing here, Sneaky Inspector?" );
+  console.log( "Hit me up at: hi@walrey.es" );
+
+  $('div#go-to-some-of-me').click(() => {
+    $('html, body').animate({
+        scrollTop: $("section#some-of-me").offset().top
+    }, 500);
+  })
+
+  $('div#back-arrow').click(() => {
+    $('div#back-arrow').parent().css("display","none");
+  })
+
+  $('a#happy-development-experience').click(() => {
+    $('div#happy-development-experience').css("display","flex");
+  })
+});
